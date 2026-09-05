30b33b56691ba7a55ca8c43e2e5551e0facc5cbb
superdev0925 <alicia0925martin@gmail.com>
2026-09-05 15:34:55 +0300

    fix: use OpenStreetMap tiles so the map does not require a Carto key

--- frontend/src/components/RouteMap.jsx
+++ frontend/src/components/RouteMap.jsx
@@ -97,7 +97,7 @@ export default function RouteMap({ plan }) {
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
         {geometry.length > 1 && (
